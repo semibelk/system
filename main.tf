bca516134b61629da4ef6f656f844c549241bf94
semiwoot <semiwoot@gmail.com>
2025-01-23 20:44:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ data "vsphere_network" "network" {
 # Virtual Machine
 resource "vsphere_virtual_machine" "vm" {
   name             = "MyNewVM"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = data.vsphere_datastore.datastore.id
 
   num_cpus = 2
