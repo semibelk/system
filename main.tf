d8f6e6ff6582cf0206e9d22756b7fa882683797d
semiwoot <semiwoot@gmail.com>
2025-01-23 20:28:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "vsphere" {
-  user           = "terraformvsphere.local"
+  user           = "terraform@vsphere.local"
   password       = "Terraform123!"
-  server         = "192.168.1.102"
+  vsphere_server         = "192.168.1.102"
   allow_unverified_ssl = true
 }
 
